7d5dd7b9d716aaeadc12b4de0a789bdfb188f340
afrilabs-capacity <factswarehouse360@gmail.com>
2022-04-07 14:19:43 +0100

    face api complete for test

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -45,7 +45,7 @@ class SubscriberController extends Controller
     {
         $command = "/var/www/facetest/faceapi/test.py";
         //return shell_exec("C:\Users\hp\AppData\Local\Programs\Python\Python36\python ${command} ${first} ${second} 2>&1");
-        return shell_exec("python3 ${command} /var/www/facetest/faceapi/${first} /var/www/facetest/faceapi/${second} 2>&1");
+        return shell_exec("python3 ${command} ${first} ${second} 2>&1");
 
 
         //  2>&1
