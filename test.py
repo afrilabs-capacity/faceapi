3ae02cd76a403ae59e37b22876f8f75f91d523b1
afrilabs-capacity <factswarehouse360@gmail.com>
2022-04-10 21:41:00 +0100

    face api complete for test

--- test.py
+++ test.py
@@ -25,9 +25,9 @@ unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 # Now we can see the two face encodings are of the same person with `compare_faces`!
 
 results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-print (results)
+print(results)
 
-#if results[0] < 0.2:
- #   print(True)
-#else:
- #   print(False)
+if results[0] < 0.2:
+    print(True)
+else:
+    print(False)
