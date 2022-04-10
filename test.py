522dd478f9aea6c224526fcdd2c3b4353058626c
afrilabs-capacity <factswarehouse360@gmail.com>
2022-04-11 00:59:29 +0100

    face api complete for test

--- test.py
+++ test.py
@@ -23,8 +23,7 @@ unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
 print(results)
 
-if results :
-    print(True)
-else:
-    print(False)
-    
+# if results :
+#     print(True)
+# else:
+#     print(False)
